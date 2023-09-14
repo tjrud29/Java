a97ffa22374d5fe89c7409448f0a5b46c4c80287
tjrud29 <143372269+tjrud29@users.noreply.github.com>
2023-09-14 17:17:17 +0900

    입출력

---
+++ chap24_입출력(IO)/src/com/kh/fileIO/File_IO_Main.java
@@ -0,0 +1,32 @@
+package com.kh.fileIO;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class File_IO_Main {
+
+	public static void main(String[] args) {
+		/*try {
+			FileWriter writer = new FileWriter("abcde.txt");//생성
+			writer.write("자바 공부 꿀잼");//파일쓰기(동시에 열기)
+			writer.close();//파일 닫기
+			
+			FileReader reader = new FileReader("abced.txt");
+			BufferedReader bufferdReader = new BufferedReader(reader);
+			
+			String line;
+			while((line = bufferdReader.readLine()) != null) {
+				
+			}
+			bufferdReader.close();//파일을 다 읽으면 닫아주기
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+}*/
+}
+}
